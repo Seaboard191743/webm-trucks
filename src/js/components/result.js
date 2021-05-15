b2e6d3df7ceb248373e5a276d2f4aa77c5119d97
Seaboard191743 <exporesource@gmail.com>
2021-05-15 12:54:39 +0300

    feat: add page title

--- src/js/components/result.js
+++ src/js/components/result.js
@@ -1,12 +1,7 @@
-import { resTitle, dropdown } from "../constants";
+import { resTitle } from "../constants";
 import data from "../../data.json";
 
 export const setResValues = () => {
   const { stock } = data;
   resTitle.innerText = `${stock.length} Results found`;
-  dropdown.forEach((item, i) =>
-    item.addEventListener("click", () => {
-      item.classList.toggle("dropdown--active");
-    })
-  );
 };
--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,4 @@
+import data from "../data.json";
 export const nav = document.querySelector(".nav");
 export const nav_menu = nav.querySelector(".nav__menu");
 export const sub_menu = nav.querySelector(".nav__subMenu");
@@ -7,3 +8,6 @@ export const result = document.querySelector(".result");
 export const resTitle = result.querySelector(".result__title");
 export const dropdown = result.querySelectorAll(".dropdown");
 export const param_body = result.querySelectorAll(".param__body");
+
+const title = document.head.querySelector("title");
+title.innerText = data.page_meta.title;
