8bcffe185a2c0ea990bde8276c5c0241a5dd8bed
Seaboard191743 <exporesource@gmail.com>
2021-05-15 13:24:54 +0300

    fix: fix config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 
@@ -15,6 +16,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "images", to: "images" }],
     }),
     new CleanWebpackPlugin(),
   ],
